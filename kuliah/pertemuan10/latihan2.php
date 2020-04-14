56dbc8836a6b3deaf1e6b8a41208cf09ba7296b1
$DAF <dafabaelah@users.noreply.github.com>
2020-04-14 00:58:44 -0400

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/latihan2.php
@@ -0,0 +1,44 @@
+<?php
+require 'functions.php';
+$mahasiwa = query("SELECT * FROM mahasiswa");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <?php $i = 1;
+    foreach ($mahasiwa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" width="60px"></td>
+        <td><?= $m['nrp']; ?></td>
+        <td><?= $m['nama']; ?></td>
+        <td><?= $m['email']; ?></td>
+        <td><?= $m['jurusan']; ?></td>
+        <td>
+          <a href="">ubah</a> | <a href="">hapus</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
